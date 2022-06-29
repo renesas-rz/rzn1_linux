foundriesio/linux

cpufreq: renesas: Add R9A06G032 to platdev driver
This adds the Renesas R9A06G032 to the cpufreq-dt-platdev driver

Signed-off-by: Michel Pollet <michel.pollet@bp.renesas.com>

drivers/cpufreq/cpufreq-dt-platdev.c
@@ -57,6 +57,7 @@ static const struct of_device_id machines[] __initconst = {
 	{ .compatible = "renesas,r8a7792", },
 	{ .compatible = "renesas,r8a7793", },
 	{ .compatible = "renesas,r8a7794", },
+	{ .compatible = "renesas,rzn1", },
 	{ .compatible = "renesas,sh73a0", },
 
 	{ .compatible = "rockchip,rk2928", },

renesas-rz/rzn1_linux

drivers/cpufreq/cpufreq-dt-platdev.c
@@ -64,6 +64,7 @@ static const struct of_device_id allowlist[] __initconst = {
 	{ .compatible = "renesas,r8a7792", },
 	{ .compatible = "renesas,r8a7793", },
 	{ .compatible = "renesas,r8a7794", },
+	{ .compatible = "renesas,r9a06g032", },
 	{ .compatible = "renesas,sh73a0", },
 
 	{ .compatible = "rockchip,rk2928", },